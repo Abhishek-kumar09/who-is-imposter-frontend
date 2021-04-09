0c1b7c69fe3f3de108353d480496e1d87201add9
ravisha7feb <ravisha7feb@gmail.com>
2021-04-09 12:51:09 +0530

    fixed styling issues

--- src/User-Dashboard/dashboard0.js
+++ src/User-Dashboard/dashboard0.js
@@ -4,17 +4,18 @@ import plus from '../assets/plus.png'
 import love from '../assets/Love.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './dashboard.css'
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 const Dashboard = () => {
     return (
         <>
+        
 
-            <div class="dashboard">
+        <div class="dashboard"> 
 
-                <div class="col-1">
+                
+                    <div class="side-bar-header">  {/*display:flex*/}
 
-                    <div class="logoAndHeader"> {/*display: flex*/}
 
                         <div class="logo">
                             <img src={logo} />
@@ -32,7 +33,11 @@ const Dashboard = () => {
 
                         </div>
 
+                        <div class="welcome">Welcome Username!</div>
+
                     </div>
+
+                    <div class="row-second"> {/*display:flex*/}
 
                     <div class="side-bar-user-card">
 
@@ -48,49 +53,37 @@ const Dashboard = () => {
                             </div>
 
                             <div class="logout-button">
-                                <button><Link to="/">LOG OUT</Link></button>
+                            <button><Link to="/">LOG OUT</Link></button>
                             </div>
 
 
                         </div>
 
                     </div>
-
-
-
-                </div>  {/*col-1 ends*/}
-
-
-                <div class="col-2">
-
-                    <div class="welcome">Welcome Username!</div>
 
                     <div class="postings">
 
-                        <div class="create-post">
+                            <div class="create-post">
+                                
+                                    <div class="create-post-1"> {/*display:flex*/}
+                                        <div class="plus-sign">
+                                            <img src={plus} />
 
-                            <div class="create-post-1"> {/*display:flex*/}
-                                <div class="plus-sign">
-                                    <img src={plus} />
+                                        </div>
+                                        <div class="text">
+                                            Write a post ....
+                                        </div>
+                                    </div>
 
+                                <div class="post-button">
+                                    <button>Post</button>
                                 </div>
-                                <div class="text">
-                                    Write a post ....
-                                </div>
+
                             </div>
 
-                            <div class="post-button">
-                                <button>Post</button>
-                            </div>
-
-                        </div>
-
-                    </div>
-                    <div>
-                    
-                        <div class="posting">
-
-                            <div class="post1"> {/*display:flex*/}
+                        <div class="people-posts">
+                        <div class="posts-post">
+                            <div class="posts-post-1"> {/*display:flex*/}
                                 {/*Get the data from backend*/}
                                 <div class="avatar">
                                 </div>
@@ -99,41 +92,34 @@ const Dashboard = () => {
                                     <p>Written Posts with blalalalal</p>
                                 </div>
                             </div>
-
-                            <div class="second-row">
-
-                            <div class="love-reacts">
                             <div class="love-react">
                                 <img src={love} alt="love-react" />
                             </div>
 
-                            <div class="love-reactions">
-                                12
-                            </div>
-                            </div>
+                        </div>
 
-                            <div class="show-more-button">
-                                <a>Show More...</a>
+                        <div class="posts-post">
+                            <div class="posts-post-1"> {/*display:flex*/}
+                                {/*Get the data from backend*/}
+                                <div class="avatar">
+                                </div>
 
+                                <div class="text">
+                                    <p>Written Posts with blalalalal</p>
+                                </div>
                             </div>
-
+                            <div class="love-react">
+                                <img src={love} alt="love-react" />
                             </div>
-                            
 
                         </div>
 
-                    </div>
-                        
-                        
-                   
+                            </div>
+                            </div>
 
-                </div>
+                            </div>
 
-
-            </div>
-
-
-
+        </div>
 
 
         </>
